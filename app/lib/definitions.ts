d7a99aee5b690c31dffee7f60d96edaa3f279f29
Jonathan <jonathandajome@gmail.com>
2025-01-24 22:01:18 -0500

    build navlinks

---
+++ app/lib/definitions.ts
@@ -0,0 +1,7 @@
+import { IconType } from "react-icons";
+
+export type Links = {
+  name: string;
+  href: string;
+  icon?: IconType;
+};
---
+++ app/ui/navbar/nav-links.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { HiHome, HiUser, HiViewGrid } from "react-icons/hi";
+import { Links } from "@/app/lib/definitions";
+
+const links: Links[] = [
+  {
+    name: "Home",
+    href: "/",
+    icon: HiHome,
+  },
+  {
+    name: "Profile",
+    href: "/profile",
+    icon: HiUser,
+  },
+  {
+    name: "Settings",
+    href: "/settings",
+    icon: HiViewGrid,
+  },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx("flex gap-2 items-center p-2", {
+              "text-white": pathname === link.href,
+              "text-gray-500": pathname !== link.href,
+            })}
+          >
+            <LinkIcon className="w-6 h-6 block md:hidden" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
