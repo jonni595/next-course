c869b4684eaf34a4e458ae803fd1dbfb00ae3c01
Jonathan <jonathandajome@gmail.com>
2025-01-24 22:03:06 -0500

    build setting

---
+++ app/settings/page.tsx
@@ -0,0 +1,10 @@
+import { roboto } from "@/app/fonts";
+
+export const metadata = {
+  title: "Settings",
+  description: "Settings page",
+};
+
+export default function Settings() {
+  return <div className={roboto.className}> Settings page</div>;
+}
