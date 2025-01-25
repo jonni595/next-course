a60b4a93265ce3627a772af41ce5067e4203f67b
Jonathan <jonathandajome@gmail.com>
2025-01-24 22:02:54 -0500

    build profile

---
+++ app/profile/page.tsx
@@ -0,0 +1,10 @@
+import { roboto } from "@/app/fonts";
+
+export const metadata = {
+  title: "Profile",
+  description: "Profile page",
+};
+
+export default function Profile() {
+  return <div className={roboto.className}> Profile page</div>;
+}
