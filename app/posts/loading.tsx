161134eabae12605ee558fa3f31957eba9144b80
Jonathan <jonathandajome@gmail.com>
2025-01-25 21:45:10 -0500

    Build skeleton

---
+++ app/posts/loading.tsx
@@ -0,0 +1,6 @@
+import { CardSkeleton } from "@/app/ui/skeleton";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <CardSkeleton />;
+}
