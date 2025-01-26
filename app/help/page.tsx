6bb6001de38eaa4b4653fb9259f1fc49b1a9917f
Jonathan <jonathandajome@gmail.com>
2025-01-25 20:53:13 -0500

    Build help page

---
+++ app/help/page.tsx
@@ -0,0 +1,10 @@
+import { roboto } from "@/app/fonts";
+
+export const metadata = {
+  title: "Help",
+  description: "Help page",
+};
+
+export default function Help() {
+  return <div className={roboto.className}> Help page</div>;
+}
