982c819f1e382f861d049ec33dca23cc66275cb8
Jonathan <jonathandajome@gmail.com>
2025-02-10 15:59:18 -0500

    Build list tasks

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,9 +1,68 @@
-
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const AddTask = () => {
-  return (
-    <div>AddTask</div>
-  )
-}
+  const router = useRouter();
 
-export default AddTask
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const cleanTask = () => {
+    setTask({ title: "", description: "" });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!task.title || !task.description) {
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+      const data = await response.json();
+      cleanTask();
+      console.log(data);
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 w-1/4">
+        <input
+          type="text"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Task"
+          value={task.title}
+          onChange={(e) => setTask({ ...task, title: e.target.value })}
+        />
+        <textarea
+          name=""
+          id=""
+          rows={3}
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Type your task here"
+          value={task.description}
+          onChange={(e) => setTask({ ...task, description: e.target.value })}
+        ></textarea>
+        <button className="bg-indigo-600 hover:bg-indigo-800 text-white px-4 py-2 rounded">
+          Add Task
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTask;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
 import { roboto } from "@/app/fonts";
+import { prisma } from "./lib/prisma";
+
+const getTasks = async () => {
+  return await prisma.task.findMany();
+};
 
 export default async function Home() {
+  const tasks = await getTasks();
+  console.log(tasks);
   return (
-    <div className={roboto.className}>
-      <h1>Tasks List</h1>
-    </div>
+    <section className="container mx-auto">
+      <div className={`roboto.className grid grid-cols-3 gap-3 mt-10`}>
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className="p-3 border border-slate-600 rounded-md hover:bg-slate-600 hover:cursor-pointer"
+          >
+            <p className="text-sm font-semibold leading-6 text-white md:uppercase">
+              {task.title}
+            </p>
+            <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+              {task.description}
+            </p>
+            <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+              {new Date(task.createdAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
